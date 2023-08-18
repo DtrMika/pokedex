24467687f019be663ebf1c4fcfeabd17c0fb2aa5
DtrMika <mikail.dutra.93@gmail.com>
2023-08-18 19:00:41 -0300

    actualizar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const PAGE_URL= `https://pokeapi.co/api/v2/pokemon/`;
+const PAGE_URL= `https://pokeapi.co/api/v2/pokemon/`;// creamos una constante para obtener la pagina
 
-fetch(PAGE_URL)
-.then((response)=>{
+fetch(PAGE_URL)// hacemos la solicitud para la url lo que nos da una respuesta
+.then((response)=>{// este primer then maneja esta respuesta de la solicitudgit 
     // aca verificamos si la respuesta es exitosa 200
     if(!response.ok){
         throw new console.error("Error en de red"+response.status);
@@ -10,10 +10,10 @@ fetch(PAGE_URL)
     return response.text();
 })
 //Maneja el contenido de la pagina
-.then((data)=>{
+.then((data)=>{ // este then maneja el contenido de la pagina
     console.log(data)
 })
 
-.catch((error)=>{
+.catch((error)=>{ // captura y maneja cualquier error que pueda ocurrir durante el proceso de solicitud
     console.error("No se logro obtener la pagina", error);
 });
