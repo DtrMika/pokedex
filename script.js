589b21fba4f16b589583ecb303be4070511cf388
DtrMika <mikail.dutra.93@gmail.com>
2023-08-20 17:11:00 -0300

    conexion con la api

--- script.js
+++ script.js
@@ -1,22 +1,61 @@
-const PAGE_URL = `https://pokeapi.co/api/v2/pokemon/`; // creamos una constante para obtener la pagina
+const div = document.getElementById("pokemons");
+const storeValue = localStorage.getItem("catID");
 
-fetch(PAGE_URL) // hacemos la solicitud para la url lo que nos da una respuesta
+const PAGE_URL = "https://pokeapi.co/api/v2/pokemon/"; // creamos una constante para obtener la pagina
+
+fetch(PAGE_URL)
   .then((response) => {
-    // este primer then maneja esta respuesta de la solicitudgit
-    // aca verificamos si la respuesta es exitosa 200
     if (!response.ok) {
-      throw new Error("Error en de red" + response.status);
+      throw new Error("Error en la red: " + response.status);
     }
-    // esto devuelve la respuesta obtenida
     return response.json();
   })
-  //Maneja el contenido de la pagina
   .then((data) => {
-    // este then maneja el contenido de la pagina
-    console.log(data);
-  })
+    const results = data.results;
 
+    // Recorre la lista de resultados y obtiene más detalles de cada Pokémon
+    const promises = results.map((pokemon) => {
+      return fetch(pokemon.url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Error en la red: " + response.status);
+          }
+          return response.json();
+        })
+        .then((pokemonData) => {
+          return {
+            name: pokemonData.name,
+            image: pokemonData.sprites.front_default,
+            description: "Descripción de tu Pokémon", // Puedes personalizar esto
+          };
+        });
+    });
+
+    // Espera a que se completen todas las solicitudes antes de mostrar los datos
+    Promise.all(promises)
+      .then((dataArray) => {
+        showData(dataArray); // Llama a la función showData para mostrar los datos
+      })
+      .catch((error) => {
+        console.error("No se pudo obtener la página", error);
+      });
+  })
   .catch((error) => {
-    // captura y maneja cualquier error que pueda ocurrir durante el proceso de solicitud
-    console.error("No se logro obtener la pagina", error);
+    console.error("No se pudo obtener la lista de Pokémon", error);
   });
+
+const container = document.getElementById("pokemons");
+
+function showData(dataArray) {
+  container.innerHTML = "";
+  for (const item of dataArray) {
+    container.innerHTML += `<div class="row card" style="width: 45%">
+      <img class="card-img-top" width="200" src="${item.image}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${item.name}</h5>
+        <p class="card-text">${item.description}<br>Precio USD: ${item.cost}<br>Vendidos: ${item.soldCount}</p>
+        <a href="#" class="btn btn-primary"></a>
+      </div>
+    </div>`;
+  }
+}
