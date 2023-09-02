c013220e5a820a73a27fc68fab28aba98b42b858
DtrMika <mikail.dutra.93@gmail.com>
2023-09-02 10:45:28 -0300

    finalizado

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const storeValue = localStorage.getItem("catID");
 const PAGE_URL = "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0"; // creamos una constante para obtener la pagina
 // const Pokemon_URL = `https://pokeapi.co/api/v2/pokemon/${mew}`
 
-
-
 async function getAll() {
-
   const response = await fetch(PAGE_URL);
 
   if (!response.ok) throw new Error(`No se encontró el endpoint`);
@@ -16,21 +13,16 @@ async function getAll() {
 
   const { results } = data;
 
-  console.log(results)
+  console.log(results);
   pokemonData = results;
 
   showData(results);
-
-
 }
 
-
-
 async function showData(dataArray) {
-
   let template = "";
 
-  const localStoragePokemon = localStorage.getItem('pokemonName');
+  const localStoragePokemon = localStorage.getItem("pokemonName");
 
   if (!localStoragePokemon) {
     for (let item of dataArray) {
@@ -56,19 +48,18 @@ async function showData(dataArray) {
   `;
     }
 
-    return container.innerHTML = template;
-
+    return (container.innerHTML = template);
   }
 
   // Si hay algo en el localStorage, muestra esto
-  const pokemon_dinamic_URL = `https://pokeapi.co/api/v2/pokemon/${localStoragePokemon}/`
+  const pokemon_dinamic_URL = `https://pokeapi.co/api/v2/pokemon/${localStoragePokemon}/`;
 
   const response = await fetch(pokemon_dinamic_URL);
-  if (!response.ok) return alert('pokemon no válido')
+  if (!response.ok) return alert("pokemon no válido");
   const data = await response.json();
 
   const { base_experience, id, sprites } = data;
-  console.log(data)
+  console.log(data);
 
   template += `
     <div class="col-12">
@@ -86,26 +77,23 @@ async function showData(dataArray) {
     </div>
 `;
 
-  return container.innerHTML = template;
-
+  return (container.innerHTML = template);
 }
 
-const inputText = document.querySelector('#txtbtn');
-const btn = document.querySelector('#btn-search');
+const inputText = document.querySelector("#txtbtn");
+const btn = document.querySelector("#btn-search");
 
-console.log(inputText)
-console.log(btn)
+console.log(inputText);
+console.log(btn);
 
 function getPokemonName() {
-  btn.addEventListener('click', () => {
+  btn.addEventListener("click", () => {
     let inputValue = inputText.value.toLowerCase();
-    if (!inputValue) return alert('pone datos')
-    localStorage.setItem('pokemonName', inputValue)
+    if (!inputValue) return alert("pone datos");
+    localStorage.setItem("pokemonName", inputValue);
     location.reload();
-  })
+  });
 }
 
-
-
-document.addEventListener('DOMContentLoaded', getAll);
-document.addEventListener('DOMContentLoaded', getPokemonName);
+document.addEventListener("DOMContentLoaded", getAll);
+document.addEventListener("DOMContentLoaded", getPokemonName);
